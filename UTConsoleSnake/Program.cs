92b81b8b0a7a9312520ead03dd930b549da9417e
kamil2789 <kamil2789@wp.pl>
2020-08-14 21:37:59 +0200

    init commit

---
+++ UTConsoleSnake/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UTConsoleSnake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
